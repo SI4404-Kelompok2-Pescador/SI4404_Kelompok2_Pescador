379a8a1924c6bfbf3f6b5f8d706848e27b288f4a
LAPTOP-J22K61CA\Muhammad Rivaldo <muhammadrivaldo276@gmail.com>
2022-12-21 13:35:10 +0700

    Add Login and Register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         {{-- load css file --}}
         <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-        <title>Login</title>
+        <title>Login</title>            
+    </head>
+
+    <body>  
         <style>
             body
             {
@@ -17,11 +20,8 @@
                 background-repeat : no-repeat;
                 background-size : cover;
             }
-        </style>
-        
-    </head>
+        </style> 
 
-    <body>     
         <div class="d-flex container mt-5 justify-content-end">
             <div class="d-flex container mt-5 justify-content-start">
             
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,8 +9,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         {{-- load css file --}}
         <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-        <title>Register</title>
+        <title>Register</title> 
+    </head>
 
+    <body>
         <style>
             body
             {
@@ -19,9 +21,6 @@
                 background-size : cover;
             }
         </style>
-    </head>
-
-    <body>
         <!-- REGISTRASI INPUT -->
         <div class="d-flex container mt-5 justify-content-end">
             <div class="d-flex container mt-5 justify-content-center bg-white bg-opacity-50" style="width: 40rem;">
