fd64b71427bf00f556676a2295882fdc5fdc5644
Faruqi Hafiz <faruqihafiz21@gmail.com>
2022-12-21 16:48:10 +0700

    feat/regiser API success

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -9,18 +9,32 @@ use Validator;
 
 class authController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $response = Http::post(env('API'). '/register', [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'address' => $request->input('address'),
+            'password' => $request->input('password'),
+            'password_confirmation' => $request->input('password_confirmation'),
+        ]);
+        $response = $response->json();
+                
+        return redirect('/login')->with('success', $response['message']);
+    }
+
+    
 
     public function login(Request $request)
     {
 
-        $response = Http::post('http://127.0.0.1:3030/api/login', [
+        $response = Http::post(env('API'). '/login', [
             'email' => $request->input('email'),
             'password' => $request->input('password'),
         ]);
         $response = $response->json();
-        // dd($response);
-        if ($response['status'] == 200) {
+        if ($response['status'] == 'success') {
             $request->session()->put('token', $response['token']);
         }
 
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -28,25 +28,26 @@
                 
                 <hr class="ms-2 me-2 mt-0 mb-0">
                 <div class="card-body">
-                    <form action="" method="post">
+                    <form action="{{route('register.post')}}" method="post">
+                        @csrf
                         <h2 class="boltext text-center m-3">Register</h2>
-                        <label class="mb-2" for="nama">Name</label><br>
-                        <input class="form-control mb-2" type="text" name="nama" id="nama" required placeholder="Your Name">
+                        <label class="mb-2" for="name">Name</label><br>
+                        <input class="form-control mb-2" type="text" name="name" id="name" required placeholder="Your Name">
                         <label class="mb-2" for="email">E-mail</label><br>
                         <input class="form-control mb-2" type="text" name="email" id="email" required placeholder="Your E-mail">
-                        <label class="mb-2" for="nohp">Phone Number</label><br>
-                        <input class="form-control mb-2" type="text" name="nohp" id="nohp" required placeholder="Phone Number">
+                        <label class="mb-2" for="phone">Phone Number</label><br>
+                        <input class="form-control mb-2" type="text" name="phone" id="phone" required placeholder="Phone Number">
                         <label class="mb-2" for="address">Address</label><br>
                         <input class="form-control mb-2" type="text" name="address" id="address" required placeholder="Your Address">
-                        <label class="mb-2" for="pass">Password</label><br>
-                        <input class="form-control mb-2" type="Password" name="pass" id="pass" required placeholder="Password">
+                        <label class="mb-2" for="password">Password</label><br>
+                        <input class="form-control mb-2" type="password" name="password" id="password" required placeholder="Password">
                         <div class="text-start pt-2 mb-2">
-                            <button class="btn btn-dark" type="submit" name="submit" onclick="" style="width: 150px">Register</button>
+                            <a href="{{'login'}}"><button type="submit" name="submit" onclick="" style="width: 150px" class="btn btn-dark">Register</button></a>
                         </div>
                     </form>
 
                     <div class="text-center d-flex justify-content-end">
-                        <p class="ME-1">Have already an account? <a href="login.php"
+                        <p class="ME-1">Have already an account? <a href="{{'login'}}"
                             class="fw-bold text-body"><u>Login here</u></a></p>
                     </div>
                 </div>
